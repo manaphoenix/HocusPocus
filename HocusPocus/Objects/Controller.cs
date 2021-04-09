3d69b9874236584c7f513f0c4432b90c759b6036
manaphoenix <toehead976@gmail.com>
2021-04-09 10:25:31 -0400

    Add project files.

---
+++ HocusPocus/Objects/Controller.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Controls;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.IO;
+using System.Linq;
+
+namespace HocusPocus.Objects
+{
+	public class Controller
+	{
+		private ObservableCollection<RandomizerItem> _Items;
+		private StringBuilder builder;
+		private readonly Random RNG = new Random(Guid.NewGuid().GetHashCode());
+
+		public ObservableCollection<RandomizerItem> MyItems
+		{
+			get { return _Items; }
+
+			set
+			{
+				if (_Items != value)
+				{
+					_Items = value;
+				}
+			}
+		}
+
+		public Controller()
+		{
+			builder = new StringBuilder();
+			MyItems = new ObservableCollection<RandomizerItem>();
+		}
+
+		public void NestedRoll(RandomizerItem item)
+		{
+			var roll = RNG.Next(0, item.Items.Count);
+			RandomizerItem result = (RandomizerItem)item.Items[roll];
+			builder.Append(result.OutputValue + "\n");
+
+			if (result.Nested)
+			{
+				NestedRoll(result);
+			}
+		}
+
+		public StringBuilder DoRoll(RandomizerItem item)
+		{
+			builder.Clear();
+
+			if (item.Items.Count < 1) return builder;
+
+			var roll = RNG.Next(0, item.Items.Count);
+			RandomizerItem result = (RandomizerItem)item.Items[roll];
+			builder.Append(result.OutputValue + "\n");
+
+			if (result.Nested)
+			{
+				NestedRoll(result);
+			}
+
+			return builder;
+		}
+
+		public void NewItem()
+		{
+			var item = new RandomizerItem();
+			MyItems.Add(item);
+		}
+
+		
+		public void Save()
+		{
+			/*
+			var items = _Items.ToArray();
+
+			var bf = new BinaryFormatter();
+			var stream = File.Open("Randomizer.dat", FileMode.Create, FileAccess.Write);
+
+			bf.Serialize(stream, items);
+			stream.Close();
+			*/
+		}
+
+		public void Load()
+		{
+			/*
+			if (File.Exists("Randomizer.dat"))
+			{
+				var bf = new BinaryFormatter();
+				var stream = File.Open("Randomizer.dat", FileMode.Open, FileAccess.Read);
+
+				var items = (RandomizerItem[])bf.Deserialize(stream);
+				stream.Close();
+
+				foreach (var item in items)
+				{
+					item.Header = item.ItemName;
+					_Items.Add(item);
+				}
+			}
+			*/
+		}
+		
+	}
+}
---
+++ HocusPocus/Objects/RandomizerItem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace HocusPocus.Objects
+{
+	[Serializable]
+	public class RandomizerItem : TreeViewItem, INotifyPropertyChanged, ISerializable
+	{
+		private string _Name;
+		private string _OutputValue;
+		private bool _Nested;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public string ItemName
+		{
+			get { return _Name; }
+
+			set
+			{
+				if (_Name != value)
+				{
+					_Name = value;
+					PropertyChange();
+				}
+			}
+		}
+
+		public string OutputValue 
+		{ 
+			get { return _OutputValue ?? _Name; }
+
+			set
+			{
+				if (_OutputValue != value)
+				{
+					_OutputValue = value;
+					PropertyChange();
+				}
+			}
+		}
+
+		public bool Nested
+		{
+			get { return _Nested; }
+
+			set
+			{
+				if (_Nested != value)
+				{
+					_Nested = value;
+					PropertyChange();
+				}
+			}
+		}
+
+		public RandomizerItem()
+		{
+			ItemName = "New Randomizer";
+			Nested = false;
+			Header = ItemName;
+			Foreground = Brushes.White;
+		}
+
+		public RandomizerItem(SerializationInfo info, StreamingContext context)
+		{
+			_Name = (string)info.GetValue("ItemName", typeof(string));
+			_OutputValue = (string)info.GetValue("OutputValue", typeof(string));
+			_Nested = (bool)info.GetValue("Nested", typeof(bool));
+		}
+
+		public void PropertyChange([CallerMemberName] string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("ItemName", _Name);
+			info.AddValue("OutputValue", _OutputValue);
+			info.AddValue("Nested", _Nested);
+		}
+	}
+}
